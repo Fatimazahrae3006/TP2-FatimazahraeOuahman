5da64b0170d7703fa9f1a908eec519680397c3d4
Fatimazahrae3006 <FATIMAZAHRAE.OUAHMAN@emsi-edu.ma>
2025-11-24 20:53:15 +0100

    feat: Add failing unit tests to demonstrate CI failure

--- app/src/test/java/com/example/devops/ExampleUnitTest.kt
+++ app/src/test/java/com/example/devops/ExampleUnitTest.kt
@@ -1,17 +1,16 @@
 package com.example.devops
 
 import org.junit.Test
-
 import org.junit.Assert.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        assertEquals(4, 2 + 1) // Test qui échoue volontairement !
+    }
+    
+    @Test  
+    fun substract_isCorrect() {
+        assertEquals(4, 8 - 3) // Test qui échoue volontairement !
     }
 }
